0b6e20a91a7cf231a9d3d61354c473651502174e
William Mouton <mouton.w2000@gmail.com>
2024-10-22 14:23:36 +0200

    init again

---
+++ src/test/java/com/example/tpjenkins/TpjenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpjenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
